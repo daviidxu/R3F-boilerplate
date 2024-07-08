badc8d04d5204afb6a1604b1ab9c9bf43e0cf110
David XU <david.xu@modiscloud.net>
2024-07-08 22:13:43 +0200

    setup canvas to access hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { Canvas } from "@react-three/fiber";
-import "./App.css";
-
 const App = () => {
   return (
-    <Canvas>
+    <>
       <mesh>
         <torusKnotGeometry />
         <meshNormalMaterial />
       </mesh>
-    </Canvas>
+    </>
   );
 };
 
